ff1840e54d1b1468f2a6ba660f7bc6b2378fcbf4
Koti Bajjuri <kotireddy.b@gmail.com>
2017-12-01 22:22:05 -0700

    Updated to new requirement to format each word in a sentense

--- src/main/java/edu/wgu/assignment/StringHelper.java
+++ src/main/java/edu/wgu/assignment/StringHelper.java
@@ -34,12 +34,12 @@ public class StringHelper {
 		
 		StringBuilder formattedStr = new StringBuilder(str.substring(0,1));
 		
-		Set<Byte> setOfDistinctChars = new HashSet<Byte>(); 
+		Set<Byte> setOfDistinctChars = new HashSet<Byte>();
 		
 		// Time complexity is O(N) of the substring because the process has to look through to find distinct.
 		for (byte b : asciiValues) {
 			//All the time complexity here is O(1) because everything here do the same logic.
-			if((b>= 65 && b<= 90) || (b>= 97 && b<= 122)) {
+			if(Character.isAlphabetic(b)) {
 				setOfDistinctChars.add(b);
 			} else {
 				//If the first character in the string itself a non-alphabet needs to append to string.
@@ -59,12 +59,69 @@ public class StringHelper {
 	}
 	
 	/**
+	 * Constructs a string with the first letter of the word, the count of distinct letter between the first and last letter, and the last letter of the word.
+	 * @param str - Str to construct with the first letter of the word, the count of distinct letter between the first and last letter, and the last letter of the word.
+	 * @return Formatted string with first letter of the word, the count of distinct letters in between first and last letter, and the last letter.
+	 */
+	public String getDistinctLettersOfEachWord(String str) throws UnsupportedEncodingException{
+		if(str==null || str.trim().isEmpty()) {
+			return "";
+		}
+		StringBuilder newString = new StringBuilder();
+		byte[] allStringBytes = str.getBytes("US-ASCII");
+		int wordStart = 0;
+		Set<Character> uniqueCharSet = new HashSet<Character>();
+		int i = 0;
+		char previousChar = (char)allStringBytes[0];
+		int currentStrLocation = 0;
+		for (byte b : allStringBytes) {
+			if(!Character.isAlphabetic(b) || currentStrLocation == allStringBytes.length-1) {
+				if(uniqueCharSet.size()>0) {
+					newString.append(uniqueCharSet.size());
+					if(currentStrLocation != allStringBytes.length-1 || !Character.isAlphabetic(b)) {
+						newString.append(previousChar);
+					}
+					uniqueCharSet.clear();
+				}
+				newString.append((char)b);
+				i=0;
+			} else {
+				if(i == wordStart) {
+					newString.append((char)b);
+				} else {
+					if(Character.isAlphabetic(allStringBytes[currentStrLocation+1])){
+						uniqueCharSet.add((char)b);
+					}
+				}
+				i++;
+				previousChar = (char)b;
+				
+			}
+			currentStrLocation++;
+		}
+		return newString.toString();
+	}
+	
+	/**
 	 * Main method to launch the String helper class.
 	 * @param args
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception{
 		StringHelper sh = new StringHelper();
-		System.out.println(sh.getDistinctLettersBetweenFirstAndLastLetters("AutoMmotive_koti"));
+		//System.out.println(sh.getDistinctLettersBetweenFirstAndLastLetters("AutoMmotive_koti"));
+		//
+		System.out.println(sh.getDistinctLettersOfEachWord("Autom_otive"));
+		System.out.println(sh.getDistinctLettersOfEachWord("Y_ooo3yesss_IAMDOING_dGOOD"));
+		System.out.println(sh.getDistinctLettersOfEachWord("Y_ooo3yesss_IAMDOINGdGOOD"));
+		
+		
+		System.out.println(sh.getDistinctLettersOfEachWord("Automotive"));
+		//$$$Autom&
+		System.out.println(sh.getDistinctLettersOfEachWord("$$$Autom&oive&"));
+		
+		
+		
+		
 	}
 }
--- src/test/java/edu/wgu/assignment/StringHelperTest.java
+++ src/test/java/edu/wgu/assignment/StringHelperTest.java
@@ -5,6 +5,8 @@ package edu.wgu.assignment;
 
 import static org.junit.Assert.*;
 
+import java.io.UnsupportedEncodingException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -29,6 +31,37 @@ public class StringHelperTest {
 	@Before
 	public void initialize() {
 		helper = new StringHelper();
+	}
+	
+	@Test
+	public void testGetDistinctLettersOfEachWord() throws UnsupportedEncodingException{
+		//Automotive have 2 t's and 2 o's makes total 6 distinctive alphabets.
+		assertTrue(helper.getDistinctLettersOfEachWord("Automotive").equals("A6e"));
+		
+		//AutomOtive have lower case and upper case O makes total 7 distinctive alphabets
+		assertTrue(helper.getDistinctLettersOfEachWord("AutomOtive").equals("A7e"));
+		
+		//Auto_motive have a special character _ makes to return 3 count of distinctive ahead and 5 count after.
+		assertTrue(helper.getDistinctLettersOfEachWord("Auto_motive").equals("A2o_m4e") );
+		
+		//Auto_98_motive have _98_ non alphabetics make it to return 3 count distinct, _98_ ignored characters and 5 count of distinctive.  
+		assertTrue(helper.getDistinctLettersOfEachWord("Auto_98_motive").equals("A2o_98_m4e") );
+		
+		//Hello have 2 distinct characters in the middle.
+		assertTrue(helper.getDistinctLettersOfEachWord("Hello").equals("H2o") );
+		
+		
+		//non-alphabet at the end returning the same with the distinctive characters in the middle with other non-alphabets.
+		assertTrue(helper.getDistinctLettersOfEachWord("Iam_doing_good&").equals("I1m_d3g_g1d&"));
+		
+		//non-alphabet at the beginning returning the same with the distinctive characters in the middle with other non-alphabets.
+		assertTrue(helper.getDistinctLettersOfEachWord("$Iam_doing_good&").equals("$I1m_d3g_g1d&"));
+		
+		assertTrue(helper.getDistinctLettersOfEachWord(null).equals(""));
+		
+		//Validating the false positive case returning with A6e but validating against A7e.
+		assertFalse(helper.getDistinctLettersOfEachWord("Automotive").equals("A7e"));
+		
 	}
 
 	/**
